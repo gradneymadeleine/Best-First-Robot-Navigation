4d900a1541fd2508517d331fa7fd7c77729d1706
Madeleine Gradney <gradney@chapman.edu>
2020-03-03 02:33:25 -0800

    have most of manhattan and a bit of a star, added the nodes

--- Search.cpp
+++ Search.cpp
@@ -12,6 +12,7 @@ Search::Search()
   cost =0;
   row =0;
   column =0;
+  node =0
 }
 Search::~Search()
 {
@@ -21,12 +22,13 @@ Search::~Search()
 void evalDist()
 
 }
-void Search::Euclidian()
+void Search::Euclidian() //stratagy 1
 {
   cin>>fileName;
   file f(fileName)
   f.getFile();
   cost = 0;
+  node =0
 
   row = f.getFileRow();
   column = f.getFileColumn();
@@ -37,11 +39,12 @@ void Search::Euclidian()
   while(g.intial_x != g.goal_x && g.intial_y != g.goal_y){
     //check where it can go in it's pos
     double tempDis =0;
-    int pos = 0;
+    int pos = -1;
     eucliDist = 1000.0;
     if (g.intial_x - 1 > -1 && (g.myGrid[g.intial_x - 1][g.intial_y] == '.' || g.myGrid[g.intial_x - 1][g.intial_y] == 'g'))
        {
            cout << "Up" << endl;
+           node++;
            tempDis = getEucliDist(g.intial_x - 1, g.intial_y, g.goal_x, g.goal_y);
            if(tempDis <= eucliDist)
            {
@@ -52,6 +55,7 @@ void Search::Euclidian()
        if (g.intial_x + 1 < row && (g.myGrid[g.intial_x + 1][g.intial_y] == '.' || g.myGrid[g.intial_x + 1][g.intial_y] == 'g'))
       {
           cout << "Down" << endl;
+          node++;
           tempDis = getEucliDist(g.intial_x + 1, g.intial_y, g.goal_x, g.goal_y);
           if (tempDis <= eucliDist)
           {
@@ -62,6 +66,7 @@ void Search::Euclidian()
       if (g.intial_y - 1 > -1 && (g.myGrid[g.intial_x][g.intial_y - 1] == '.' || g.myGrid[g.intial_x][g.intial_y - 1] == 'g'))
       {
           cout << "Left" << endl;
+          node++;
           tempDis = getEucliDist(g.intial_x, g.intial_y - 1, g.goal_x, g.goal_y);
           if (tempDis <= eucliDist)
           {
@@ -72,6 +77,7 @@ void Search::Euclidian()
       if (g.intial_y + 1 < row && (g.myGrid[g.intial_x][g.intial_y + 1] == '.' || g.myGrid[g.intial_x][g.intial_y + 1] == 'g'))
       {
           cout << "Right" << endl;
+          node++;
           tempDis = getEucliDist(g.intial_x, g.intial_y + 1, g.goal_x, g.goal_y);
           if (tempDis <= eucliDist)
           {
@@ -107,18 +113,103 @@ void Search::Euclidian()
        g.printGrid();
        cost++;
        cout << "Cost: " << cost << endl;
+       cout << "Node: " << node << endl;
    }
 
 }
 
-void Search::Manhattan(){
+void Search::Manhattan(){ //stratgy 3
   cin >> fileName;
   file f(fileName);
   f.getFile();
   cost =0;
+  node =0;
 
+  Grid g(row,column);
+  g.fileOp(fileName);
+
+  double tempDis =0;
+  int pos = -1;
+  manhatDist = 1000.0;
+
+  if (g.intial_x - 1 > -1 && (g.myGrid[g.intial_x - 1][g.intial_y] == '.' || g.myGrid[g.intial_x - 1][g.intial_y] == 'g'))
+       {
+           cout << "Up" << endl;
+           node++;
+           tempDis = getManhatDist(g.intial_x - 1, g.intial_y, g.final_x, g.final_y);
+           if (tempDis <= manhatDist)
+           {
+               manhatDist = tempDis;
+               pos = 0;
+           }
+       }
+       if (g.intial_x + 1 < row && (g.myGrid[g.intial_x + 1][g.intial_y] == '.' || g.myGrid[g.intial_x + 1][g.intial_y] == 'g'))
+       {
+           cout << "Down" << endl;
+           node++;
+           tempDis = getManhatDist(g.intial_x + 1, g.intial_y, g.final_x, g.final_y);
+           if (tempDis <= manhatDist)
+           {
+               manhatDist = tempDis;
+               pos = 1;
+           }
+       }
+       if (g.intial_y - 1 > -1 && (g.myGrid[g.intial_x][g.intial_y - 1] == '.' || g.myGrid[g.intial_x][g.intial_y - 1] == 'g'))
+       {
+           cout << "Left" << endl;
+           node++;
+           tempDis = getManhatDist(g.intial_x, g.intial_y - 1, g.final_x, g.final_y);
+           if (tempDis <= manhatDist)
+           {
+               manhatDist = tempDis;
+               pos = 2;
+           }
+       }
+       if (g.intial_y + 1 < row && (g.myGrid[g.intial_x][g.intial_y + 1] == '.' || g.myGrid[g.intial_x][g.intial_y + 1] == 'g'))
+       {
+           cout << "Right" << endl;
+           node++;
+           tempDis = getManhatDist(g.intial_x, g.intial_y + 1, g.final_x, g.final_y);
+           if (tempDis <= manhatDist)
+           {
+               manhatDist = tempDis;
+               pos = 3;
+           }
+       }
+
+       if (pos == 0)
+       {
+           g.myGrid[g.intial_x][g.intial_y] = 'O';
+           g.myGrid[g.intial_x - 1][g.intial_y] = 'i';
+           g.intial_x = g.intial_x - 1;
+       }
+       else if (pos == 1)
+       {
+           g.myGrid[g.intial_x][g.intial_y] = 'O';
+           g.myGrid[g.intial_x + 1][g.intial_y] = 'i';
+           g.intial_x = g.intial_x + 1;
+       }
+       else if (pos == 2)
+       {
+           g.myGrid[g.intial_x][g.intial_y] = 'O';
+           g.myGrid[g.intial_x][g.intial_y - 1] = 'i';
+           g.intial_y = g.intial_y - 1;
+       }
+       else if (pos == 3)
+       {
+           g.myGrid[g.intial_x][g.intial_y] = 'O';
+           g.myGrid[g.intial_x][g.intial_y + 1] = 'i';
+           g.intial_y = g.intial_y + 1;
+       }
+
+       g.printGrid();
+       cost++;
+       cout << "Cost: " << cost << endl;
+       cout << "Node: " << node << endl;
+   }
 }
 
+void Search::A
 
 //formulas for Euclidian and Manhattan distance
 double Search:: getEcliDist(int intial_x, int intial_y,int goal_x, int goal_y);
